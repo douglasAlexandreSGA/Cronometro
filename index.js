4c79a7d3756eb407bb9514c1168f11acf573dbd0
Douglas Alexandre <douglas.alexandre716@gmail.com>
2022-07-07 14:11:42 -0300

    adição de horas no Cronometro

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-let sec = 0
-let min = 0
-var interval
+let hor = 0;
+let sec = 0;
+let min = 0;
+var interval;
 
 function start() {
-   interval = setInterval(watch,1000)
-
+  interval = setInterval(watch, 1000);
 }
 
 function pause() {
-    clearInterval(interval)
+  clearInterval(interval);
 }
 
 function stop() {
-    clearInterval(interval)
-    sec=0
-    min=0
-    document.getElementById('watch-t').innerText='00:00'
+  clearInterval(interval);
+  sec = 0;
+  min = 0;
+  document.getElementById("watch-t").innerText = "00:00:00";
 }
 
-
-
 function watch() {
-    sec++
-    if(sec==60){
-        min++
-        sec=0
+  sec++;
+  if (sec == 60) {
+    min++;
+    sec = 0;
+    if (min == 60) {
+      hor++;
+      min = 0;
     }
-    document.getElementById('watch-t').innerText=min+":"+sec
+  }
+  document.getElementById("watch-t").innerText = hor + ":" + min + ":" + sec;
 }
